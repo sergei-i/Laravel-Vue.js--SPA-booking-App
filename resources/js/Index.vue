9b530eb9744112652eb68516b5667033d96c5e04
Sergei <1161441@gmail.com>
2020-06-08 17:36:31 +0200

    Add basket component

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -2,6 +2,10 @@
     <div>
         <nav class="navbar bg-white border-bottom navbar-light">
             <router-link class="navbar-brand mr-auto" :to="{name: 'home'}">Home</router-link>
+            <router-link class="btn nav-button" :to="{name: 'basket'}">
+                Basket
+                <span v-if="itemsInBasket" class="badge badge-secondary">{{ itemsInBasket }}</span>
+            </router-link>
         </nav>
         <div class="container mt-4 mb-4 pr-4 pl-4">
             <router-view></router-view>
@@ -9,7 +13,14 @@
     </div>
 </template>
 <script>
+    import {mapGetters} from 'vuex';
+
     export default {
-        name: 'Index'
+        name: 'Index',
+        computed: {
+            ...mapGetters({
+                itemsInBasket: 'itemsInBasket'
+            })
+        }
     }
 </script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,28 +2,34 @@ import VueRouter from 'vue-router';
 import Bookables from './bookables/Bookables';
 import Bookable from './bookable/Bookable';
 import Review from './review/Review';
+import Basket from './basket/Basket';
 
 const routes = [
     {
         path: '/',
         component: Bookables,
-        name: 'home',
+        name: 'home'
     },
     {
         path: '/bookable/:id',
         component: Bookable,
-        name: 'bookable',
+        name: 'bookable'
     },
     {
         path: '/review/:id',
         component: Review,
-        name: 'review',
+        name: 'review'
     },
+    {
+        path: '/basket',
+        component: Basket,
+        name: 'basket'
+    }
 ]
 
 const router = new VueRouter({
     mode: 'history',
-    routes,
+    routes
 })
 
 export default router;
